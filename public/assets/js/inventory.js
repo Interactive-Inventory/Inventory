455c1d7bb3217dad8fd11539f6f72bf7b23d5364
Thea Long <delong2@ncsu.edu>
2018-05-09 17:27:25 -0400

    new files

---
+++ public/assets/js/inventory.js
@@ -0,0 +1,16 @@
+ 
+// Nav bar function
+ // $(document).ready(function(){
+ //    $('.sidenav').sidenav();
+ //  });
+
+
+// <script type="text/javascript">
+$(document).ready(function() {
+    $("select").material_select();
+})
+
+$(document).ready(function() {
+    $(".button-collapse").sideNav();
+})
+// </script>
