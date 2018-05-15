b95af3cedb7f9d511d289934099a22d486410839
earyzhikova <elenaryzh55@gmail.com>
2018-05-15 14:03:42 -0400

    Add db

--- config/connection.js
+++ config/connection.js
@@ -6,6 +6,9 @@
 var Sequelize = require("sequelize");
 
 // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+}else{
 var sequelize = new Sequelize("sequelize_inventory", "root", "4488Pikes!", {
   host: "localhost",
   dialect: "mysql",
@@ -15,6 +18,7 @@ var sequelize = new Sequelize("sequelize_inventory", "root", "4488Pikes!", {
     idle: 10000
   }
 });
+};
 
 // Exports the connection for other files to use
 module.exports = sequelize;
