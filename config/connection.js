eb9d5562a3152171543ac5c70220456791f81f7a
earyzhikova <elenaryzh55@gmail.com>
2018-05-16 13:59:24 -0400

    Connection db

--- config/connection.js
+++ config/connection.js
@@ -4,15 +4,15 @@
 
 // Dependencies
 var Sequelize = require("sequelize");
-// var mysql = require("mysql");
+var mysql = require("mysql");
 
 
 // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-// if(process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDB_URL)
-// }else{
-var sequelize = new Sequelize("gio7ee463sfiz67d", "ak9wyela9zuhcjnm", "hllx06w8jz246i2g", {
-  host: "gp96xszpzlqupw4k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env[config.use_env_variable])
+}else{
+var sequelize = new Sequelize("sequelize_inventory", "root", "4488Pikes!", {
+  host: "localhost",
   dialect: "mysql",
   pool: {
     max: 5,
@@ -20,7 +20,7 @@ var sequelize = new Sequelize("gio7ee463sfiz67d", "ak9wyela9zuhcjnm", "hllx06w8j
     idle: 10000
   }
 });
-// };
+};
 
 // Exports the connection for other files to use
 module.exports = sequelize;
