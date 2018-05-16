eadaffffc910c91b665b5adfdd10bbc5394f0c20
earyzhikova <elenaryzh55@gmail.com>
2018-05-16 14:28:21 -0400

    H

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,6 @@
 var Sequelize = require("sequelize");
 var mysql = require("mysql");
 var config = require(__dirname + "/config.json");
-console.log(config);
 
 
 // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
@@ -14,15 +13,15 @@ if(config.use_env_variable) {
   console.log(config.use_env_variable)
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 }else{
-  var sequelize = new Sequelize("gio7ee463sfiz67d", "ak9wyela9zuhcjnm", "hllx06w8jz246i2g", {
-  host: "gp96xszpzlqupw4k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
-});
+  var sequelize = new Sequelize("sequelize_inventory", "root", "4488Pikes!", {
+    host: "localhost",
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000
+    }
+  });
 };
 
 // Exports the connection for other files to use
